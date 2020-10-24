ab4f9ef293e71fdb4ecbe1b8d065bc6384205429
stunner19 <singhkaranpal81@gmail.com>
2020-10-24 23:31:59 +0530

    Added dense layer

---
+++ create_dense_eval.py
@@ -0,0 +1,27 @@
+def create_dense(layer_sizes):
+    model = Sequential()
+    model.add(Dense(layer_sizes[0], activation='sigmoid', input_shape=(image_size,)))
+
+    for s in layer_sizes[1:]:
+        model.add(Dense(units = s, activation = 'sigmoid'))
+
+    model.add(Dense(units=num_classes, activation='softmax'))
+    return model
+
+def evaluate(model, batch_size=128, epochs=5):
+    model.summary()
+    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=.1, verbose=False)
+    loss, accuracy  = model.evaluate(x_test, y_test, verbose=False)
+    
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['training', 'validation'], loc='best')
+    plt.show()
+
+    print()
+    print(f'Test loss: {loss:.3}')
+    print(f'Test accuracy: {accuracy:.3}')
